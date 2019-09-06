02243c9e994d47e05a28490556014875a4d54783
Isaac <isaac@ch1ll.ax>
2019-09-06 13:45:24 -0700

    Added mobile nav and no parallax on mobile

--- static/index.js
+++ static/index.js
@@ -3,19 +3,38 @@ $(document).ready(function () {
 
   $(window).on('load resize scroll', function() {
     var headerHeight = $("#header").offset().top + $("#header").outerHeight() - 1;
+
     if($(window).scrollTop() > headerHeight && $(window).width() > 576){
       $("#smallNav").fadeIn(100);
       if (!disable_scroll && $(window).scrollTop() >= $('#about').offset().top && $(window).scrollTop() < $('#portfolio').offset().top) {
         $('#smallNav a.active').removeClass('active');
         $(`a[href$='about']`, '#smallNav').addClass('active');
       }
-      if (!disable_scroll && $(window).scrollTop() >= $('#portfolio').offset().top && $(window).scrollTop() < $('#contact').offset().top) {
+      if (!disable_scroll && $(window).scrollTop() >= $('#portfolio').offset().top - $('#smallNav').outerHeight() && $(window).scrollTop() < $('#contact').offset().top) {
         $('#smallNav a.active').removeClass('active');
         $(`a[href$='portfolio']`, '#smallNav').addClass('active');
       }
-      if (!disable_scroll && $(window).scrollTop() >= $('#contact').offset().top) {
+      if (!disable_scroll && $(window).scrollTop() >= $('#contact').offset().top - $('#smallNav').outerHeight()) {
         $('#smallNav a.active').removeClass('active');
         $(`a[href$='contact']`, '#smallNav').addClass('active');
+      }
+    }
+    else if($(window).width() <= 576){
+      if(!disable_scroll && $(window).scrollTop() < $('#about').offset().top ) {
+        $('.overlay-content a.active').removeClass('active');
+        $(`a[href$='header']`, '.overlay-content').addClass('active');
+      }
+      if (!disable_scroll && $(window).scrollTop() >= $('#about').offset().top && $(window).scrollTop() < $('#portfolio').offset().top) {
+        $('.overlay-content a.active').removeClass('active');
+        $(`a[href$='about']`, '.overlay-content').addClass('active');
+      }
+      if (!disable_scroll && $(window).scrollTop() >= $('#portfolio').offset().top && $(window).scrollTop() < $('#contact').offset().top) {
+        $('.overlay-content a.active').removeClass('active');
+        $(`a[href$='portfolio']`, '.overlay-content').addClass('active');
+      }
+      if (!disable_scroll && $(window).scrollTop() >= $('#contact').offset().top) {
+        $('.overlay-content a.active').removeClass('active');
+        $(`a[href$='contact']`, '.overlay-content').addClass('active');
       }
     }
     else{
@@ -30,6 +49,26 @@ $(document).ready(function () {
     $(`a[href$='${id}']`, '#smallNav').addClass('active');
     disable_scroll = true;
     $('html,body').animate({scrollTop: $(id).offset().top},'slow', function(){disable_scroll = false;});
-    
   });
+
+  $('.overlay-content a').click(function (e){
+    e.preventDefault();
+    var id = $(this).attr("href");
+    $('.overlay-content a.active').removeClass('active');
+    $(`a[href$='${id}']`, '.overlay-content').addClass('active');
+    $('.mobileNavOpen').fadeIn(200)
+    $(".overlay").fadeOut(200);
+    disable_scroll = true;
+    $('html,body').animate({scrollTop: $(id).offset().top},'slow', function(){disable_scroll = false;});
+  });
+
+  $('.mobileNavOpen').click(function (e){
+    $(this).fadeOut(200)
+    $(".overlay").fadeIn(200);
+  })
+
+  $('.mobileNavClose').click(function (e){
+    $('.mobileNavOpen').fadeIn(200)
+    $(".overlay").fadeOut(200);
+  })
 });
